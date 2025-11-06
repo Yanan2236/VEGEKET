49ee281af703224631c99d1a717bcc96d078562e
Yanan2236 <kent31415926535@gmail.com>
2025-11-06 14:30:59 +0000

    feat: implemented a payment feature useing Stripe

--- config/urls.py
+++ config/urls.py
@@ -21,9 +21,17 @@ from base import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     
+    # Items
     path('', views.IndexListView.as_view()), 
     path('items/<str:pk>/', views.ItemDetailView.as_view()),
+    
+    # Cart
     path('cart/', views.CartListView.as_view()),
     path('cart/add/', views.AddCartView.as_view()),
-    path('cart/remove/<str:pk>/', views.remove_from_cart)
+    path('cart/remove/<str:pk>/', views.remove_from_cart),
+    
+    # Pay
+    path('pay/checkout/', views.PayWithStripe.as_view()),
+    path('pay/success/', views.PaySuccessView.as_view()),
+    path('pay/cancel/', views.PayCancelView.as_view()),
 ]
